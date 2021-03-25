04679533e29eeb970b294901a533a8c06c699144
soredake <5204968+soredake@users.noreply.github.com>
2021-03-25 11:19:20 +0200

    init

--- setup.cmd
+++ setup.cmd
@@ -1,26 +1,29 @@
 @echo off
 
-# mpv
+rem mpv
 del %APPDATA%\mpv\mpv.conf
 del %APPDATA%\mpv\input.conf
 mkdir %APPDATA%\mpv
 mklink /h %APPDATA%\mpv\mpv.conf .\mpv.conf
 mklink /h %APPDATA%\mpv\input.conf .\input.conf
 
-# zsh
+rem zsh
 del C:\tools\msys64\home\user\.zshrc
 mkdir C:\tools\msys64\home\user
 mklink /h C:\tools\msys64\home\user\.zshrc .\.zshrc
 
-# git
+rem git
 del %USERPROFILE%\.gitconfig
 mklink /h %USERPROFILE%\.gitconfig .\.gitconfig
 mklink /h C:\tools\msys64\home\user\.gitconfig .\.gitconfig
 
-# microsoft windows terminal
+rem microsoft windows terminal
 del %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 mkdir %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
 mklink /h %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json .\mswinterminal.json
 
-powershell 'Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service'
-runas /user:Administator "powershell Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service"
+rem powershell 'Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service'
+rem runas /user:Administator "powershell Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service"
+
+
+mklink /j C:\tools\msys64\home\user\.ssh %USERPROFILE%\.ssh
