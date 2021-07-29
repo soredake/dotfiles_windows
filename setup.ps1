65d2f2dbaaca3230a9ffa457ede1ea5fcba1ecc2
soredake <5204968+soredake@users.noreply.github.com>
2021-07-29 21:16:38 +0300

    ddd

--- setup.ps1
+++ setup.ps1
@@ -4,15 +4,14 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 # https://docs.chocolatey.org/en-us/choco/setup#install-with-powershell.exe
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-#choco feature enable -n=useRememberedArgumentsForUpgrades
 # install my packages
-choco install -y steam-cleaner steam-client 7zip chocolateygui keepassxc powertoys telegram ds4windows qbittorrent discord goggalaxy autoruns choco-cleaner epicgameslauncher viber edgedeflector jdownloader vscode python nodejs yarn git hackfont microsoft-windows-terminal msys2 visualstudio2019buildtools google-backup-and-sync nomacs mpv tor-browser wiztree ubisoft-connect zeal caffeine rclone parsec protonvpn youtube-dl ppsspp steelseries-engine firefox crystaldiskinfo.install spotify mpvnet.install borderlessgaming
+choco install -y steam-cleaner steam-client 7zip.install chocolateygui keepassxc powertoys telegram ds4windows qbittorrent discord goggalaxy autoruns choco-cleaner epicgameslauncher viber edgedeflector jdownloader vscode.install python nodejs.install yarn git.install hackfont microsoft-windows-terminal msys2 visualstudio2019buildtools nomacs mpv tor-browser wiztree ubisoft-connect zeal caffeine rclone parsec protonvpn youtube-dl ppsspp steelseries-engine firefox crystaldiskinfo.install spotify mpvnet.install borderlessgaming doublecmd google-drive-file-stream coretemp eartrumpet
 choco install -y retroarch --params '/DesktopShortcut'
 #choco install -y pcsx2.install --params '/Desktop'
 choco install -y origin --params '/DesktopIcon' # ea desktop?
 choco install -y rpcs3 --pre
 choco install -y choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:15:00'"
-ForEach ($app in 'viber','steam-client','firefox','origin','telegram','discord','rpcs3','ds4windows','ubisoft-connect','tor-browser','vscode') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
+ForEach ($app in 'viber','steam-client','firefox','origin','telegram','discord','rpcs3','ds4windows','ubisoft-connect','tor-browser','vscode.install') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
 winget install LogMeIn.Hamachi
 winget install HandyOrg.HandyWinget-GUI
 winget install BlueStack.BlueStacks
@@ -45,7 +44,7 @@ New-Item -ItemType Junction -Path "C:\tools\msys64\home\user\.ssh" -Target "$env
 # mpv
 Remove-Item -Path "$env:APPDATA\mpv\*.conf"
 mkdir "$env:APPDATA\mpv\scripts"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/pause-when-minimize.lua" -OutFile "$env:APPDATA\mpv\scripts\pause-when-minimize.lua"
+#Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/pause-when-minimize.lua" -OutFile "$env:APPDATA\mpv\scripts\pause-when-minimize.lua"
 New-Item -ItemType SymbolicLink -Path "$env:APPDATA\mpv\mpv.conf" -Target ".\mpv.conf"
 New-Item -ItemType SymbolicLink -Path "$env:APPDATA\mpv\input.conf" -Target ".\input.conf"
 # zsh
@@ -65,14 +64,11 @@ yarn set version berry
 # better to install python with winget once https://github.com/microsoft/winget-cli/issues/219 and https://github.com/microsoft/winget-cli/issues/212 is resolved
 setx PATH "$env:PATH;$env:APPDATA\Python\Python39\Scripts"
 
-# fix https://github.com/microsoft/WSL/issues/4103
-compact /U "$env:USERPROFILE\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu20.04onWindows_79rhkp1fndgsc\LocalState"
-
-# fix https://github.com/microsoft/WSL/issues/5336#issuecomment-770494713
-Set-Content -Path "$env:USERPROFILE\.wslconfig" -Value "[wsl2]`nswap=0"
-
 # https://stackoverflow.com/questions/30496116/how-to-disable-hyper-v-in-command-line
 bcdedit /set hypervisorlaunchtype off
 
-# mpv.net config # keepaspect-window
-Add-Content -Path "$env:APPDATA\mpv.net\mpv.conf" -Value "`nno-keepaspect-window`nslang = 'eng,en'"
+# mpv.net config
+#Add-Content -Path "$env:APPDATA\mpv.net\mpv.conf" -Value "`nno-keepaspect-window"
+
+# https://mspscripts.com/disable-windows-10-fast-boot-via-powershell/
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d "0" /f
