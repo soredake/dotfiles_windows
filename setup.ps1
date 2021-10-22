2f19425353bafb4ccfcee7fb99bdc0ef9347e648
soredake <5204968+soredake@users.noreply.github.com>
2021-10-22 09:31:21 +0300

    gf

--- setup.ps1
+++ setup.ps1
@@ -6,24 +6,28 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 # install my packages
 # delete youtube-dl when new mpv.net version is comes
-choco install -y steam-cleaner steam-client 7zip.install chocolateygui keepassxc powertoys telegram.install ds4windows qbittorrent discord goggalaxy autoruns choco-cleaner epicgameslauncher viber edgedeflector jdownloader python nodejs.install yarn git.install hackfont microsoft-windows-terminal msys2 visualstudio2019buildtools nomacs mpv.install tor-browser wiztree ubisoft-connect zeal.install rclone.portable parsec protonvpn youtube-dl ppsspp steelseries-engine firefox crystaldiskinfo.install spotify mpvnet.install borderlessgaming doublecmd syncplay google-drive-file-stream coretemp eartrumpet megasync obs-studio stretchly victoria msiafterburner dxwnd wincdemu ffmpeg winaero-tweaker.install adb wsl-ubuntu-2004 wsl2 yt-dlp
-choco install -y retroarch --params '/DesktopShortcut'; choco install -y origin --params '/DesktopIcon'; choco install -y rpcs3 --pre
+# wsl-ubuntu-2004 wsl2 microsoft-windows-terminal
+choco install -y steam-cleaner steam-client 7zip.install chocolateygui keepassxc powertoys telegram.install ds4windows qbittorrent discord goggalaxy autoruns choco-cleaner epicgameslauncher viber edgedeflector jdownloader python nodejs.install yarn git.install hackfont msys2 visualstudio2019buildtools nomacs mpv.install tor-browser wiztree ubisoft-connect zeal.install rclone.portable parsec protonvpn youtube-dl ppsspp steelseries-engine firefox crystaldiskinfo.install spotify mpvnet.install borderlessgaming doublecmd google-drive-file-stream coretemp eartrumpet megasync obs-studio stretchly victoria msiafterburner dxwnd wincdemu ffmpeg winaero-tweaker.install adb yt-dlp gsudo
+choco install -y retroarch --params '/DesktopShortcut'; choco install -y --ignore-checksums origin --params '/DesktopIcon'; choco install -y rpcs3 syncplay --pre
 #choco install -y pcsx2.install --params '/Desktop'
 choco install -y choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:15:00'"
 ForEach ($app in 'viber','steam-client','firefox','origin','telegram.install','discord.install','rpcs3','ds4windows','ubisoft-connect','tor-browser','goggalaxy','steelseries-engine') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
-winget install LogMeIn.Hamachi; winget install HandyOrg.HandyWinget-GUI; winget install BlueStack.BlueStacks; winget install ElectronicArts.EADesktop; winget install BiSS.WSLDiskShrinker; winget install Microsoft.VisualStudioCode; winget install kapitainsky.RcloneBrowser; winget install TomWatson.BreakTimer
+winget install -h LogMeIn.Hamachi; winget install -h HandyOrg.HandyWinget-GUI; winget install -h  BlueStack.BlueStacks; winget install -h ElectronicArts.EADesktop; winget install -h BiSS.WSLDiskShrinker; winget install -eh Microsoft.VisualStudioCode; winget install -h kapitainsky.RcloneBrowser; winget install -h TomWatson.BreakTimer
 pip install --user -U internetarchive
 
 # https://docs.microsoft.com/en-us/windows/wsl/install-win10
 # for now i use `choco install wsl-ubuntu-2004 wsl2` as i don't have new anough windows
-# wsl --install -d Ubuntu
+wsl --install -d Ubuntu
 
 # https://richardballard.co.uk/ssh-keys-on-windows-10/
+Add-WindowsCapability -Online -Name OpenSSH.Client
+#Add-WindowsCapability -Online -Name OpenSSH.Server
 Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
 
 # https://haali.su/winutils/
 Invoke-WebRequest -Uri "https://haali.su/winutils/lswitch.exe" -OutFile "$env:USERPROFILE/lswitch.exe"
 schtasks /create /tn "switch language with right ctrl" /sc onlogon /rl highest /tr "$env:USERPROFILE\lswitch.exe 163"
+schtasks /run /tn "switch language with right ctrl"
 
 # git for windows uses wrong ssh binary which leads to errors like `Permission Denied (publickey)` because it don't use windows ssh-agent
 # https://github.com/PowerShell/Win32-OpenSSH/wiki/Setting-up-a-Git-server-on-Windows-using-Git-for-Windows-and-Win32_OpenSSH#on-client
@@ -36,7 +40,7 @@ C:\tools\msys64\mingw64.exe pacman.exe -S --noconfirm zsh fish python diffutils 
 # config files, git
 Remove-Item -Path "$env:USERPROFILE\.gitconfig"
 New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig" -Target ".\.gitconfig"
-New-Item -ItemType SymbolicLink -Path "C:\tools\msys64\home\user\.gitconfig" -Target ".\.itconfig"
+New-Item -ItemType SymbolicLink -Path "C:\tools\msys64\home\user\.gitconfig" -Target ".\.gitconfig"
 # ssh
 New-Item -ItemType Junction -Path "C:\tools\msys64\home\user\.ssh" -Target "$env:USERPROFILE\.ssh"
 # mpv
@@ -58,13 +62,14 @@ New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.Wind
 cd ~; yarn set version berry
 
 # add python to path, better to install python with winget once https://github.com/microsoft/winget-cli/issues/219 and https://github.com/microsoft/winget-cli/issues/212 is resolved
-setx PATH "$env:PATH;$env:APPDATA\Python\Python39\Scripts"
+setx PATH "$env:PATH;$env:APPDATA\Python\Python310\Scripts"
 
 # https://mspscripts.com/disable-windows-10-fast-boot-via-powershell/
 # leave disabled if you use dualboot or wifi adapters
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d "0" /f
+#reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d "0" /f
 
 # trakt tv sync
+# TODO: use full path
 pip install --user -U pipx
 pipx ensurepath
 pipx install trakt-scrobbler
