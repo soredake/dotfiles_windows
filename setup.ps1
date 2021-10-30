2a46be55585644618bbf223139935c05cefd25d3
soredake <5204968+soredake@users.noreply.github.com>
2021-10-30 20:15:53 +0300

    dsfds

--- setup.ps1
+++ setup.ps1
@@ -7,12 +7,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # install my packages
 # delete youtube-dl when new mpv.net version is comes
 # wsl-ubuntu-2004 wsl2 microsoft-windows-terminal
-choco install -y steam-cleaner steam-client 7zip.install chocolateygui keepassxc powertoys telegram.install ds4windows qbittorrent discord goggalaxy autoruns choco-cleaner epicgameslauncher viber edgedeflector jdownloader python nodejs.install yarn git.install hackfont msys2 visualstudio2019buildtools nomacs mpv.install tor-browser wiztree ubisoft-connect zeal.install rclone.portable parsec protonvpn youtube-dl ppsspp steelseries-engine firefox crystaldiskinfo.install spotify mpvnet.install borderlessgaming doublecmd google-drive-file-stream coretemp eartrumpet megasync obs-studio stretchly victoria msiafterburner dxwnd wincdemu ffmpeg winaero-tweaker.install adb yt-dlp gsudo
+choco install -y steam-cleaner steam-client 7zip.install chocolateygui keepassxc powertoys telegram.install ds4windows qbittorrent discord goggalaxy autoruns choco-cleaner epicgameslauncher viber edgedeflector jdownloader python nodejs.install yarn git.install hackfont msys2 visualstudio2019buildtools nomacs mpv.install tor-browser wiztree ubisoft-connect zeal.install rclone.portable parsec protonvpn youtube-dl ppsspp steelseries-engine firefox crystaldiskinfo.install spotify mpvnet.install borderlessgaming doublecmd google-drive-file-stream coretemp eartrumpet megasync obs-studio stretchly victoria msiafterburner dxwnd wincdemu minecraft-launcher ffmpeg winaero-tweaker.install adb yt-dlp gsudo
 choco install -y retroarch --params '/DesktopShortcut'; choco install -y --ignore-checksums origin --params '/DesktopIcon'; choco install -y rpcs3 syncplay --pre
 #choco install -y pcsx2.install --params '/Desktop'
 choco install -y choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:15:00'"
 ForEach ($app in 'viber','steam-client','firefox','origin','telegram.install','discord.install','rpcs3','ds4windows','ubisoft-connect','tor-browser','goggalaxy','steelseries-engine') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
-winget install -h LogMeIn.Hamachi; winget install -h HandyOrg.HandyWinget-GUI; winget install -h  BlueStack.BlueStacks; winget install -h ElectronicArts.EADesktop; winget install -h BiSS.WSLDiskShrinker; winget install -eh Microsoft.VisualStudioCode; winget install -h kapitainsky.RcloneBrowser; winget install -h TomWatson.BreakTimer
+winget install KDE.Dolphin; winget install -h LogMeIn.Hamachi; winget install -h HandyOrg.HandyWinget-GUI; winget install -h BlueStack.BlueStacks; winget install -h ElectronicArts.EADesktop; winget install -h BiSS.WSLDiskShrinker; winget install -eh Microsoft.VisualStudioCode; winget install -h kapitainsky.RcloneBrowser; winget install -h TomWatson.BreakTimer
 pip install --user -U internetarchive
 
 # https://docs.microsoft.com/en-us/windows/wsl/install-win10
@@ -20,9 +20,9 @@ pip install --user -U internetarchive
 wsl --install -d Ubuntu
 
 # https://richardballard.co.uk/ssh-keys-on-windows-10/
-Add-WindowsCapability -Online -Name OpenSSH.Client
+#Add-WindowsCapability -Online -Name OpenSSH.Client
 #Add-WindowsCapability -Online -Name OpenSSH.Server
-Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
+#Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
 
 # https://haali.su/winutils/
 Invoke-WebRequest -Uri "https://haali.su/winutils/lswitch.exe" -OutFile "$env:USERPROFILE/lswitch.exe"
@@ -71,14 +71,18 @@ setx PATH "$env:PATH;$env:APPDATA\Python\Python310\Scripts"
 # trakt tv sync
 # TODO: use full path
 pip install --user -U pipx
-pipx ensurepath
+python -m pipx ensurepath
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # https://stackoverflow.com/questions/17794507/powershell-reload-the-path-in-powershell
 pipx install trakt-scrobbler
 trakts auth
 trakts config set players.monitored mpv
-trakts config set fileinfo.whitelist E:\
+trakts config set fileinfo.whitelist F:\
 trakts config set general.enable_notifs False
 "input-ipc-server=\\.\pipe\mpvsocket`n" + (Get-Content "$env:APPDATA\mpv.net\mpv.conf" -Raw) | Set-Content "$env:APPDATA\mpv.net\mpv.conf"
 trakts config set players.mpv.ipc_path \\.\pipe\mpvsocket
 
 # fix https://github.com/msys2/MSYS2-packages/issues/138#issuecomment-775316680
 #C:\tools\msys64\mingw64.exe bash.exe -c 'mkpasswd > /etc/passwd; mkgroup > /etc/group; sed -i "s/db//g" /etc/nsswitch.conf'
+
+# configure WSL2
+bash.exe -c 'sudo apt update && sudo apt upgrade -y && sudo apt install -y python3-pip && pip install --user -U internetarchive'
