84badf9998ef1b7d61d12bf4db56f3efd552578f
soredake <5204968+soredake@users.noreply.github.com>
2022-06-29 21:24:34 +0300

    update

--- setup.ps1
+++ setup.ps1
@@ -9,22 +9,21 @@ Invoke-WebRequest -Uri "https://www.free-codecs.com/download_soft.php?d=0ca6d164
 Add-AppPackage -Path "$env:TEMP/hevc_extension.appx"
 
 # install my packages
-# TODO: github rate limiting 429
 # TODO: https://github.com/microsoft/vscode/issues/147408 is caused by installing vscode in user mode with admin rights
 # nsudo is needed until https://github.com/gerardog/gsudo/issues/136 is done
-choco install -y hamachi spotify nsudo steam-client keepassxc powertoys ds4windows qbittorrent discord.install goggalaxy autoruns choco-cleaner viber jdownloader nodejs.install nomacs tor-browser wiztree zeal.install rclone.portable parsec protonvpn ppsspp firefox doublecmd google-drive-file-stream itch msiafterburner steascree.install ffmpeg adb yt-dlp gsudo nerdfont-hack tor
-choco install pcsx2-dev -y --params "/Desktop /UseQt" --pre; choco install -y git.install --params "/NoShellHereIntegration /NoOpenSSH"; choco install -y retroarch --params '/DesktopShortcut'; choco install -y --ignore-checksums origin --params '/DesktopIcon'; choco install -y rpcs3 syncplay --pre; choco install -y choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:15:00'"
-ForEach ($app in 'viber','steam-client','firefox','origin','discord.install','rpcs3','tor-browser','goggalaxy','pcsx2-dev') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
-$packages='BlueStack.BlueStacks','9pmz94127m4g','microsoft.powershell','BiSS.WSLDiskShrinker','Microsoft.VisualStudioCode','TomWatson.BreakTimer','Python.Python.3','9n64sqztb3lm','rammichael.7+TaskbarTweaker.Beta','64Gram.64Gram','mcmilk.7zip-zstd','stromcon.emusak','AnthonyBeaumont.AchievementWatcher','oh-my-posh','SteamGridDB.RomManager'
+choco install -y hamachi nsudo steam-client keepassxc powertoys ds4windows goggalaxy autoruns choco-cleaner viber jdownloader nodejs.install nomacs tor-browser wiztree zeal.install rclone.portable parsec protonvpn ppsspp doublecmd google-drive-file-stream itch msiafterburner steascree.install ffmpeg adb yt-dlp nerdfont-hack tor
+choco install pcsx2-dev -y --params "/Desktop /UseQt" --pre; choco install -y git.install --params "/NoShellHereIntegration /NoOpenSSH"; choco install -y --ignore-checksums origin --params '/DesktopIcon'; choco install -y rpcs3 syncplay --pre; choco install -y choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:15:00'"
+ForEach ($app in 'viber','steam-client','origin','rpcs3','tor-browser','goggalaxy','pcsx2-dev') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
+$packages='9NFH4HJG2Z9H','9NCBCSZSJRSB','9NZVDKPMR9RD','XPDC2RH70K22MN','Libretro.RetroArch','gerardog.gsudo','BlueStack.BlueStacks','9pmz94127m4g','9MZ1SNWT0N5D','BiSS.WSLDiskShrinker','Microsoft.VisualStudioCode','TomWatson.BreakTimer','Python.Python.3','9n64sqztb3lm','rammichael.7+TaskbarTweaker.Beta','64Gram.64Gram','mcmilk.7zip-zstd','stromcon.emusak','AnthonyBeaumont.AchievementWatcher','JanDeDobbeleer.OhMyPosh','SteamGridDB.RomManager'
 foreach ($package in $packages) { winget install -h --accept-package-agreements --accept-source-agreements $package }
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # https://stackoverflow.com/a/31845512 https://github.com/microsoft/winget-cli/issues/222
 pip install internetarchive "git+https://github.com/arecarn/dploy.git"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module -Name posh-git,npm-completion,yarn-completion,Terminal-Icons
+Install-Module -Name posh-git,npm-completion,yarn-completion,Terminal-Icons,PSAdvancedShortcut
 wsl --install --no-launch
-# https://yarnpkg.com/getting-started/install https://nodejs.org/dist/latest/docs/api/corepack.html
-cd ~; corepack enable; yarn set version stable
+cd ~; corepack enable; yarn set version stable # https://yarnpkg.com/getting-started/install https://nodejs.org/dist/latest/docs/api/corepack.html
 bash wsl.sh
+curl --create-dirs --remote-name-all --output-dir $env:APPDATA\mpv.net\scripts "https://codeberg.org/jouni/mpv_sponsorblock_minimal/raw/branch/master/sponsorblock_minimal.lua" "https://raw.githubusercontent.com/fbriere/mpv-scripts/master/scripts/tree-profiles.lua" "https://raw.githubusercontent.com/fbriere/mpv-scripts/master/scripts/brace-expand.lua"
 
 # https://haali.su/winutils/
 Invoke-WebRequest -Uri "https://haali.net/winutils/lswitch.exe" -OutFile $HOME/lswitch.exe
@@ -34,9 +33,8 @@ schtasks /run /tn "switch language with right ctrl"
 # setup dotfiles
 Remove-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 mkdir $HOME\Documents\PowerShell
-mkdir $env:APPDATA\mpv.net\scripts
-# TODO: --no-folding flag for dploy/psdotfiles https://news.ycombinator.com/item?id=7927930
-dploy stow dotfiles $HOME
+# https://github.com/ralish/PSDotFiles/issues/12 https://github.com/arecarn/dploy/issues/8
+sudo dploy stow dotfiles $HOME
 
 # cleanup https://docs.microsoft.com/en-us/windows/application-management/provisioned-apps-windows-client-os https://pureinfotech.com/view-installed-apps-powershell-windows-10/
 $debloat='MicrosoftTeams_8wekyb3d8bbwe','Microsoft.Todos_8wekyb3d8bbwe','Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe','Microsoft.Getstarted_8wekyb3d8bbwe','Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe','Microsoft.ZuneMusic_8wekyb3d8bbwe','Microsoft.WindowsCamera_8wekyb3d8bbwe','Microsoft.ZuneVideo_8wekyb3d8bbwe','Microsoft.WindowsMaps_8wekyb3d8bbwe','Microsoft.Windows.Photos_8wekyb3d8bbwe','Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe','Microsoft.People_8wekyb3d8bbwe','Microsoft.OneDriveSync_8wekyb3d8bbwe','Microsoft.BingWeather_8wekyb3d8bbwe','Microsoft.BingNews_8wekyb3d8bbwe','AdvancedMicroDevicesInc-2.AMDLink_0a9344xs7nr4m','Microsoft.GetHelp_8wekyb3d8bbwe','Microsoft.549981C3F5F10_8wekyb3d8bbwe',"BlueStacks X",'Microsoft.OneDrive','microsoft.windowscommunicationsapps_8wekyb3d8bbwe',"windows web experience pack"
@@ -45,31 +43,16 @@ foreach ($package in $debloat) { winget uninstall -h $package}
 # gsudo
 sudo config CacheMode Auto
 
-# shortcuts, https://stackoverflow.com/a/31815367
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\RTSS.lnk") # TODO: propose creating a link for it here https://github.com/HunterZ/choco
-$Shortcut.TargetPath = "C:\Program Files (x86)\RivaTuner Statistics Server\RTSS.exe"
-$Shortcut.Save()
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\BreakTimer - disable.lnk")
-$Shortcut.TargetPath = "$env:LOCALAPPDATA\Programs\breaktimer\BreakTimer.exe"
-$Shortcut.Arguments = "disable"
-$Shortcut.Save()
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\BreakTimer - enable.lnk")
-$Shortcut.TargetPath = "$env:LOCALAPPDATA\Programs\breaktimer\BreakTimer.exe"
-$Shortcut.Arguments = "enable"
-$Shortcut.Save()
+# shortcuts
+New-Shortcut -Name RTSS -Path $HOME\Desktop -Target "C:\Program Files (x86)\RivaTuner Statistics Server\RTSS.exe" # https://github.com/HunterZ/choco/issues/6
+New-Shortcut -Name 'BreakTimer - disable' -Path $HOME\Desktop -Target "$env:LOCALAPPDATA\Programs\breaktimer\BreakTimer.exe" -Arguments disable
+New-Shortcut -Name 'BreakTimer - enable' -Path $HOME\Desktop -Target "$env:LOCALAPPDATA\Programs\breaktimer\BreakTimer.exe" -Arguments enable
 
 # add tor service, https://gitlab.torproject.org/tpo/core/tor/-/issues/17145
-New-Service -Name "tor" -BinaryPathName '"C:\ProgramData\chocolatey\lib\tor\tools\Tor\tor.exe --nt-service -f C:\Users\User\git\dotfiles_windows\torrc"'
+sudo New-Service -Name "tor" -BinaryPathName '"C:\ProgramData\chocolatey\lib\tor\tools\Tor\tor.exe --nt-service -f C:\Users\User\AppData\Local\tor\torrc"'
 
 # backup task
 Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute "C:\Users\User\AppData\Local\Microsoft\WindowsApps\wt.exe" -Argument "--title Backup pwsh -c backup") -TaskName "Backup everything" -Settings (New-ScheduledTaskSettingsSet -StartWhenAvailable -RunOnlyIfNetworkAvailable) -Trigger (New-ScheduledTaskTrigger -Weekly -At 12:00 -DaysOfWeek 3)
-
-# https://codeberg.org/jouni/mpv_sponsorblock_minimal
-Invoke-WebRequest -Uri "https://codeberg.org/jouni/mpv_sponsorblock_minimal/raw/branch/master/sponsorblock_minimal.lua" -OutFile $env:APPDATA\mpv.net\scripts\sponsorblock_minimal.lua
-# https://github.com/fbriere/mpv-scripts/tree/master/scripts
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fbriere/mpv-scripts/master/scripts/tree-profiles.lua" -OutFile $env:APPDATA\mpv.net\scripts\tree-profiles.lua
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fbriere/mpv-scripts/master/scripts/brace-expand.lua" -OutFile $env:APPDATA\mpv.net\scripts\brace-expand.lua
 
 # https://admx.help/?Category=Windows_8.1_2012R2&Policy=Microsoft.Policies.WindowsLogon::DisableStartupSound https://aka.ms/AAh46ae
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'DisableStartupSound' -Value 1 -Force
@@ -98,4 +81,4 @@ Invoke-WebRequest -Uri "https://github.com/Romanitho/Winget-AutoUpdate/archive/r
 Expand-Archive "$env:TEMP/Winget-AutoUpdate.zip" -DestinationPath "$env:TEMP"
 pwsh "$env:TEMP/Winget-AutoUpdate-main\Winget-AutoUpdate-Install.ps1" -NotificationLevel None -UpdatesInterval Weekly
 Set-ScheduledTask -TaskName Winget-AutoUpdate -Trigger (New-ScheduledTaskTrigger -Weekly -At 12:00 -DaysOfWeek 2)
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Romanitho/Winget-AutoUpdate/574194c76441c6d835ecf70c58597ffda5b16247/excluded_apps.txt" -OutFile C:\ProgramData\Winget-AutoUpdate\excluded_apps.txt
+Invoke-WebRequest -Uri "https://gist.github.com/soredake/f0c63deeaf104e30135a28c3238a6008/raw" -OutFile C:\ProgramData\Winget-AutoUpdate\excluded_apps.txt
