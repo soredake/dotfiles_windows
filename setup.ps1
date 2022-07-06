bd0755b90cde026dc77489414952697bd88e1e61
soredake <5204968+soredake@users.noreply.github.com>
2022-07-06 20:02:16 +0300

    fsdfsd

--- setup.ps1
+++ setup.ps1
@@ -1,21 +1,15 @@
 # https://stackoverflow.com/a/31602095
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-# https://docs.chocolatey.org/en-us/choco/setup#install-with-powershell.exe
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# https://www.free-codecs.com/download/hevc_video_extension.htm
-Invoke-WebRequest -Uri "https://www.free-codecs.com/download_soft.php?d=0ca6d1645416c69a1655fb4af4e2d6ab&s=1024&r=&f=hevc_video_extension.htm" -OutFile "$env:TEMP/hevc_extension.appx"
-Add-AppPackage -Path "$env:TEMP/hevc_extension.appx"
-
 # install my packages
 # TODO: https://github.com/microsoft/vscode/issues/147408 is caused by installing vscode in user mode with admin rights
 # nsudo is needed until https://github.com/gerardog/gsudo/issues/136 is done
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y hamachi nsudo steam-client keepassxc powertoys ds4windows goggalaxy autoruns choco-cleaner viber jdownloader nodejs.install nomacs tor-browser wiztree zeal.install rclone.portable parsec protonvpn ppsspp doublecmd google-drive-file-stream itch msiafterburner steascree.install ffmpeg adb yt-dlp nerdfont-hack tor
 choco install pcsx2-dev -y --params "/Desktop /UseQt" --pre; choco install -y git.install --params "/NoShellHereIntegration /NoOpenSSH"; choco install -y --ignore-checksums origin --params '/DesktopIcon'; choco install -y rpcs3 syncplay --pre; choco install -y choco-upgrade-all-at --params "'/WEEKLY:yes /DAY:SUN /TIME:15:00'"
 ForEach ($app in 'viber','steam-client','origin','rpcs3','tor-browser','goggalaxy','pcsx2-dev') { choco pin add -n="$app"} # https://github.com/chocolatey/choco/issues/1607
-$packages='9NFH4HJG2Z9H','9NCBCSZSJRSB','9NZVDKPMR9RD','XPDC2RH70K22MN','Libretro.RetroArch','gerardog.gsudo','BlueStack.BlueStacks','9pmz94127m4g','9MZ1SNWT0N5D','BiSS.WSLDiskShrinker','Microsoft.VisualStudioCode','TomWatson.BreakTimer','Python.Python.3','9n64sqztb3lm','rammichael.7+TaskbarTweaker.Beta','64Gram.64Gram','mcmilk.7zip-zstd','stromcon.emusak','AnthonyBeaumont.AchievementWatcher','JanDeDobbeleer.OhMyPosh','SteamGridDB.RomManager'
-foreach ($package in $packages) { winget install -h --accept-package-agreements --accept-source-agreements $package }
+$packages='qBittorrent.qBittorrent','9NCBCSZSJRSB','9NZVDKPMR9RD','XPDC2RH70K22MN','Libretro.RetroArch','gerardog.gsudo','BlueStack.BlueStacks','9pmz94127m4g','Microsoft.PowerShell','BiSS.WSLDiskShrinker','Microsoft.VisualStudioCode','TomWatson.BreakTimer','Python.Python.3','9n64sqztb3lm','rammichael.7+TaskbarTweaker.Beta','64Gram.64Gram','mcmilk.7zip-zstd','stromcon.emusak','AnthonyBeaumont.AchievementWatcher','JanDeDobbeleer.OhMyPosh','SteamGridDB.RomManager'
+foreach ($package in $packages) { winget install -h --accept-package-agreements --accept-source-agreements $package } # https://github.com/microsoft/winget-cli/issues/219
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # https://stackoverflow.com/a/31845512 https://github.com/microsoft/winget-cli/issues/222
 pip install internetarchive "git+https://github.com/arecarn/dploy.git"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
@@ -24,11 +18,19 @@ wsl --install --no-launch
 cd ~; corepack enable; yarn set version stable # https://yarnpkg.com/getting-started/install https://nodejs.org/dist/latest/docs/api/corepack.html
 bash wsl.sh
 curl --create-dirs --remote-name-all --output-dir $env:APPDATA\mpv.net\scripts "https://codeberg.org/jouni/mpv_sponsorblock_minimal/raw/branch/master/sponsorblock_minimal.lua" "https://raw.githubusercontent.com/fbriere/mpv-scripts/master/scripts/tree-profiles.lua" "https://raw.githubusercontent.com/fbriere/mpv-scripts/master/scripts/brace-expand.lua"
-
+# https://www.free-codecs.com/download/hevc_video_extension.htm
+Invoke-WebRequest -Uri "https://www.free-codecs.com/download_soft.php?d=0ca6d1645416c69a1655fb4af4e2d6ab&s=1024&r=&f=hevc_video_extension.htm" -OutFile "$env:TEMP/hevc_extension.appx"
+Add-AppPackage -Path "$env:TEMP/hevc_extension.appx"
 # https://haali.su/winutils/
 Invoke-WebRequest -Uri "https://haali.net/winutils/lswitch.exe" -OutFile $HOME/lswitch.exe
 schtasks /create /tn "switch language with right ctrl" /sc onlogon /rl highest /tr "$HOME\lswitch.exe 163"
 schtasks /run /tn "switch language with right ctrl"
+# winget autoupdate https://github.com/microsoft/winget-cli/issues/212, after winget gains auto-update support install pwsh from msstore https://github.com/PowerShell/PowerShell/issues/17232
+Invoke-WebRequest -Uri "https://github.com/Romanitho/Winget-AutoUpdate/archive/refs/heads/main.zip" -OutFile "$env:TEMP/Winget-AutoUpdate.zip"
+Expand-Archive "$env:TEMP/Winget-AutoUpdate.zip" -DestinationPath "$env:TEMP"
+sudo pwsh "$env:TEMP/Winget-AutoUpdate-main\Winget-AutoUpdate-Install.ps1" -NotificationLevel None -UpdatesInterval Weekly
+sudo pwsh -c 'Set-ScheduledTask -TaskName Winget-AutoUpdate -Trigger (New-ScheduledTaskTrigger -Weekly -At 12:00 -DaysOfWeek 2)'
+sudo Invoke-WebRequest -Uri "https://gist.github.com/soredake/f0c63deeaf104e30135a28c3238a6008/raw" -OutFile C:\ProgramData\Winget-AutoUpdate\excluded_apps.txt
 
 # setup dotfiles
 Remove-Item -Path "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
@@ -60,8 +62,7 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization' /v 'NoLockScreen' /t REG_DWORD /d 1 /f
 # explorer tweaks https://stackoverflow.com/a/8110982/4207635
 $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-Set-ItemProperty $key Hidden 1
-Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key Hidden 1; Set-ItemProperty $key HideFileExt 0
 # https://www.ghacks.net/2021/10/22/how-to-remove-chat-in-windows-11/
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Chat" /f /v ChatIcon /t REG_DWORD /d 3
 # https://winitpro.ru/index.php/2021/12/16/udalit-chat-microsoft-teams-v-windows/ https://www.outsidethebox.ms/21375/ https://aka.ms/AAh4nac
@@ -75,10 +76,5 @@ New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Dnscache\Paramet
 Disable-ScheduledTask "Microsoft\Windows\Management\Provisioning\Logon"
 # cleanup scheduled tasks https://aka.ms/AAh3y2n
 ForEach ($task in "Achievement Watcher Upgrade OnLogon","OneDrive*") { Unregister-ScheduledTask "$task" -Confirm:$false }
-
-# winget autoupdate https://github.com/microsoft/winget-cli/issues/212
-Invoke-WebRequest -Uri "https://github.com/Romanitho/Winget-AutoUpdate/archive/refs/heads/main.zip" -OutFile "$env:TEMP/Winget-AutoUpdate.zip"
-Expand-Archive "$env:TEMP/Winget-AutoUpdate.zip" -DestinationPath "$env:TEMP"
-pwsh "$env:TEMP/Winget-AutoUpdate-main\Winget-AutoUpdate-Install.ps1" -NotificationLevel None -UpdatesInterval Weekly
-Set-ScheduledTask -TaskName Winget-AutoUpdate -Trigger (New-ScheduledTaskTrigger -Weekly -At 12:00 -DaysOfWeek 2)
-Invoke-WebRequest -Uri "https://gist.github.com/soredake/f0c63deeaf104e30135a28c3238a6008/raw" -OutFile C:\ProgramData\Winget-AutoUpdate\excluded_apps.txt
+# stop device connect/remove sound
+Remove-Item -Path 'HKCU:\AppEvents\Schemes\Apps\.Default\DeviceConnect\.Current' -Force; Remove-Item -Path 'HKCU:\AppEvents\Schemes\Apps\.Default\DeviceDisconnect\.Current' -Force
