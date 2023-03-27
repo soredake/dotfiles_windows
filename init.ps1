3ee469f7e9f263cdbfea5317ed5e1663aceb501e
soredake <5204968+soredake@users.noreply.github.com>
2023-03-27 19:33:46 +0300

    fixes 3

--- init.ps1
+++ init.ps1
@@ -10,10 +10,10 @@ Move-Item –Path $env:t\* -Destination $env:r
 . $env:r\function.ps1
 
 # winget install -h --accept-package-agreements --accept-source-agreements Microsoft.PowerShell
-Start-Process powershell -ArgumentList "-c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" -Verb runAs
-reloadenv
 # replace pwsh with store versions once WUA is not needed anymore
-Start-Process powershell -ArgumentList "-c choco install -y gsudo; choco install -y --pin powershell" -Verb runAs
+Start-Process -Wait powershell -ArgumentList "-c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" -Verb runAs
+reloadenv
+Start-Process -Wait powershell -ArgumentList "-c choco install -y gsudo; choco install -y --pin powershell" -Verb runAs
 reloadenv
 sudo config CacheMode Auto
 
