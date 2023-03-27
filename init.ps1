897284f750930e10fe0f2425c59c038dafb876a7
soredake <5204968+soredake@users.noreply.github.com>
2023-03-27 18:52:27 +0300

    ffdfsf

--- init.ps1
+++ init.ps1
@@ -1,10 +1,11 @@
 $env:r = "$HOME\git\dotfiles_windows"
 $env:z = "$env:TEMP/dotfiles.zip"
+$env:t = ~\git\dotfiles_windows-master
 
 New-Item -Path $env:r -ItemType Directory
 Invoke-WebRequest -Uri "https://github.com/soredake/dotfiles_windows/archive/refs/heads/master.zip" -OutFile $env:z
 Expand-Archive $env:z -DestinationPath ~/git
-Move-Item –Path ~\git\dotfiles_windows-master -Destination $env:r
+Move-Item –Path $env:t\* -Destination $env:r
 . $env:r\function.ps1
 reloadenv
 
@@ -15,3 +16,4 @@ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 pwsh $env:r\setup.ps1
 
 Remove-Item -Path $env:z
+Remove-Item -Path $env:t -Force
