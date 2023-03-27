5c7c556d216a72af625d654ef425c2b0d1537323
soredake <5204968+soredake@users.noreply.github.com>
2023-03-27 18:56:03 +0300

    fixes 2

--- init.ps1
+++ init.ps1
@@ -1,18 +1,18 @@
 $env:r = "$HOME\git\dotfiles_windows"
 $env:z = "$env:TEMP/dotfiles.zip"
-$env:t = ~\git\dotfiles_windows-master
+$env:t = "$HOME\git\dotfiles_windows-master"
 
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 New-Item -Path $env:r -ItemType Directory
 Invoke-WebRequest -Uri "https://github.com/soredake/dotfiles_windows/archive/refs/heads/master.zip" -OutFile $env:z
 Expand-Archive $env:z -DestinationPath ~/git
 Move-Item –Path $env:t\* -Destination $env:r
 . $env:r\function.ps1
-reloadenv
 
 # replace it with store versions once WUA is not needed anymore
 winget install -h --accept-package-agreements --accept-source-agreements Microsoft.PowerShell
+reloadenv
 
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 pwsh $env:r\setup.ps1
 
 Remove-Item -Path $env:z
