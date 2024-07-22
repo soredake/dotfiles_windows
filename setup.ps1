6b34a3f992a6aab433d25a5144767ec6afc423f2
soredake <5204968+soredake@users.noreply.github.com>
2024-07-23 00:57:15 +0300

    dasdadsadsdasdasads

--- setup.ps1
+++ setup.ps1
@@ -46,12 +46,13 @@ Import-Module gsudoModule
 # Suggest ways to get the most out of Windows…: WhatsNewInWindows -Disable
 # Show the Windows welcome experience…: WindowsWelcomeExperience -Hide
 # Get tips and suggestions when using Windows…: WindowsTips -Disable
-sudo -w -n --close {
-  # Downloading Sophia Script
-  Remove-Item -Path "$HOME\Downloads\Sophia*" -Recurse -Force
-  Invoke-WebRequest script.sophia.team -useb | Invoke-Expression
-  pwsh ~\Downloads\Sophia*\Sophia.ps1 -Function "CreateRestorePoint", "TaskbarSearch -Hide", "ControlPanelView -LargeIcons", "FileTransferDialog -Detailed", "ShortcutsSuffix -Disable", "UnpinTaskbarShortcuts -Shortcuts Edge, Store", "DNSoverHTTPS -Enable -PrimaryDNS 1.1.1.1 -SecondaryDNS 1.0.0.1", "Hibernation -Disable", "ThumbnailCacheRemoval -Disable", "SaveRestartableApps -Enable", "WhatsNewInWindows -Disable", "UpdateMicrosoftProducts -Enable", "InputMethod -English"
-}
+# sudo -w -n --close {
+#   # Downloading Sophia Script
+#   Remove-Item -Path "$HOME\Downloads\Sophia*" -Recurse -Force
+#   Invoke-WebRequest script.sophia.team -useb | Invoke-Expression
+#   pwsh ~\Downloads\Sophia*\Sophia.ps1 -Function "CreateRestorePoint", "TaskbarSearch -Hide", "ControlPanelView -LargeIcons", "FileTransferDialog -Detailed", "ShortcutsSuffix -Disable", "UnpinTaskbarShortcuts -Shortcuts Edge, Store", "DNSoverHTTPS -Enable -PrimaryDNS 1.1.1.1 -SecondaryDNS 1.0.0.1", "Hibernation -Disable", "ThumbnailCacheRemoval -Disable", "SaveRestartableApps -Enable", "WhatsNewInWindows -Disable", "UpdateMicrosoftProducts -Enable", "InputMethod -English"
+# }
+sudo -n pwsh $PSScriptRoot\sophia-script.ps1
 
 Invoke-Gsudo -ArgumentList "$PSScriptRoot" {
   # Jellyfin.Server cannot be installed silently https://github.com/jellyfin/jellyfin-server-windows/issues/109
