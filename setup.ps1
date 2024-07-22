08ddf1dbbdd5dc4452a0acd4014919727a43bc66
soredake <5204968+soredake@users.noreply.github.com>
2024-07-23 00:26:04 +0300

    more fixes

--- setup.ps1
+++ setup.ps1
@@ -42,17 +42,17 @@ New-Item -ItemType HardLink -Path "$env:LOCALAPPDATA\Packages\Microsoft.DesktopA
 Remove-Item -Path "$HOME\Downloads\Sophia*" -Recurse -Force
 Invoke-WebRequest script.sophia.team -useb | Invoke-Expression
 
+# https://aka.ms/AAh4e0n https://aka.ms/AAftbsj https://aka.ms/AAd9j9k https://aka.ms/AAoal1u
+# https://www.outsidethebox.ms/22048/
+# Suggest ways to get the most out of Windows…: WhatsNewInWindows -Disable
+# Show the Windows welcome experience…: WindowsWelcomeExperience -Hide
+# Get tips and suggestions when using Windows…: WindowsTips -Disable
+sudo { ~\Downloads\Sophia*\Sophia.ps1 -Function "CreateRestorePoint", "TaskbarSearch -Hide", "ControlPanelView -LargeIcons", "FileTransferDialog -Detailed", "ShortcutsSuffix -Disable", "UnpinTaskbarShortcuts -Shortcuts Edge, Store", "DNSoverHTTPS -Enable -PrimaryDNS 1.1.1.1 -SecondaryDNS 1.0.0.1", "Hibernation -Disable", "ThumbnailCacheRemoval -Disable", "SaveRestartableApps -Enable", "WhatsNewInWindows -Disable", "UpdateMicrosoftProducts -Enable", "InputMethod -English" }
+
 # NOTE: Invoke-Gsudo is used because of this https://github.com/gerardog/gsudo/issues/364
 Import-Module gsudoModule
 
 Invoke-Gsudo -ArgumentList "$PSScriptRoot" {
-  # https://aka.ms/AAh4e0n https://aka.ms/AAftbsj https://aka.ms/AAd9j9k https://aka.ms/AAoal1u
-  # https://www.outsidethebox.ms/22048/
-  # Suggest ways to get the most out of Windows…: WhatsNewInWindows -Disable
-  # Show the Windows welcome experience…: WindowsWelcomeExperience -Hide
-  # Get tips and suggestions when using Windows…: WindowsTips -Disable
-  sudo { pwsh ~\Downloads\Sophia*\Sophia.ps1 -Function "CreateRestorePoint", "TaskbarSearch -Hide", "ControlPanelView -LargeIcons", "FileTransferDialog -Detailed", "ShortcutsSuffix -Disable", "UnpinTaskbarShortcuts -Shortcuts Edge, Store", "DNSoverHTTPS -Enable -PrimaryDNS 1.1.1.1 -SecondaryDNS 1.0.0.1", "Hibernation -Disable", "ThumbnailCacheRemoval -Disable", "SaveRestartableApps -Enable", "WhatsNewInWindows -Disable", "UpdateMicrosoftProducts -Enable", "InputMethod -English" }
-
   # Jellyfin.Server cannot be installed silently https://github.com/jellyfin/jellyfin-server-windows/issues/109
   winget install --accept-package-agreements --accept-source-agreements Jellyfin.Server
 
