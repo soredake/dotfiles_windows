4ac35919307d1dc8911b3ab47d922ea7478cf2aa
soredake <5204968+soredake@users.noreply.github.com>
2026-01-19 12:32:57 +0200

    19.01.2026

--- scripts/upgrade-all.ps1
+++ scripts/upgrade-all.ps1
@@ -1,5 +1,6 @@
 trakts stop
 # 'powershell' 'node' 'scoop' 'chocolatey' moved to unigetui
+# uv/pipx: https://github.com/marticliment/UniGetUI/issues/1301 https://github.com/marticliment/UniGetUI/issues/3696
 topgrade --no-retry --cleanup --yes --only 'uv'
 # uv tool upgrade --all
 trakts start --restart
